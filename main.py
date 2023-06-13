20553f7674e56b67597be62c999765fd6fd86425
Krishna <krishnasarathishem@gmail.com>
2023-06-13 13:46:32 +0530

    Create and style display label

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ root = Tk()
 root.minsize(500, 700)
 root.maxsize(500, 700)
 
+display = Label(root, text='0', font="monospace 25 bold", background='#517059', width=22, height=2,anchor='w')
+display.grid(columnspan=4, row=6, pady=35, padx=23)
 
 
 root.mainloop()
