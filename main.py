a7be9a44809dcb15ec7367884975b9862a422f00
Krishna <krishnasarathishem@gmail.com>
2023-06-14 19:39:53 +0530

    Delete width of display

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ root = Tk()
 root.minsize(500, 700)
 root.maxsize(500, 700)
 
-display = Label(root, text='0', font="monospace 25 bold", background='#517059', width=22, height=2, anchor='w')
+display = Label(root, text='0', font="monospace 25 bold", background='#517059', height=2, anchor='w')
 display.grid(columnspan=4, row=0, pady=35, padx=23)
-
 
 root.mainloop()
