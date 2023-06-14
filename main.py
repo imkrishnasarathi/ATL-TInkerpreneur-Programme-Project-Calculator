c7b5cff8a0a4f35709e14b914e896b168b874f75
Krishna <krishnasarathishem@gmail.com>
2023-06-14 19:53:11 +0530

    Edit display width and add top row buttons

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ root = Tk()
 root.minsize(500, 700)
 root.maxsize(500, 700)
 
-display = Label(root, text='0', font="monospace 25 bold", background='#517059', height=2, anchor='w')
+display = Label(root, text='0', font="monospace 25 bold", background='#517059', height=2, anchor='w', width=22)
 display.grid(columnspan=4, row=0, pady=35, padx=23)
 
-buttons = Frame(root, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=1)
+buttons = Frame(root, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=1, width=200, height=100)
 buttons.grid(columnspan=4, row=1)
+
+btnClear = Button(buttons, text='C', width=30, height=3)
+btnClear.grid(row=0, columnspan=3)
+
+btnDivide = Button(buttons, text='/', height=3, width=10)
+btnDivide.grid(row=0, column=3)
 
 
 
